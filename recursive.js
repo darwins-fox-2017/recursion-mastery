035b5e46d08fb7a0678219d50768c4e8cb5fff6b
Diky Arga & Bambang Rahmadi Kurniawan Payu <pair+dikyarga.idbmb@hacktiv8.com>
2017-01-30 19:58:49 +0700

    complete recursion release 0 and 1

--- recursive.js
+++ recursive.js
@@ -6,6 +6,16 @@
 
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  var i = 2;
+  while (i <= angka) {
+    if (angka % i === 0) {
+      angka /= i
+      hasil.push(i)
+    } else {
+      i++
+    }
+  }
+  return hasil
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,6 +30,18 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+  if (number.toString().length === 1) {
+    return number
+  } else {
+    var temp = 1;
+      for (var i = 0; i < number.toString().length; i++) {
+        // console.log(number.toString()[i]);
+        temp *= number.toString()[i]
+      }
+      number = temp
+      return simple_recursive(number)
+  }
+
 }
 
 console.log(simple_recursive(39))  // 4
