ea3a6edb4b70b18ffe7e75d630f8192db4249ff8
Agindo <ramadhanagindo@gmail.com>
2017-02-03 09:34:17 +0700

    yg 2 selesai

--- recursive.js
+++ recursive.js
@@ -19,7 +19,17 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 // Release 1
 
 let simple_recursive = (number) => {
-  // write your code here
+  var a = number.toString();
+  var d = 1;
+  if(a.length !== 1){
+    for(var b=0;b<a.length;b++){
+      var c = Number(a[b]);
+      d = d * c;
+    }
+    return simple_recursive(d);
+  }else{
+    return number;
+  }
 }
 
 console.log(simple_recursive(39))  // 4
